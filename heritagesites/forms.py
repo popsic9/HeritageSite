e2499a71ae29a23f37eceae1ffe9da0bddcdd245
sunche <sunche@umich.edu>
2018-11-06 16:32:58 -0500

    Add

---
+++ heritagesites/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from heritagesites.models import HeritageSite
+
+class HeritageSiteForm(forms.ModelForm):
+	justification = forms.CharField(required=False)
+	date_inscribed = forms.IntegerField(required=False)
+	longitude = forms.DecimalField(required=False)
+	latitude = forms.DecimalField(required=False)
+	area_hectares = forms.DecimalField(required=False)
+
+
+	class Meta:
+		model = HeritageSite
+		fields = '__all__'
+
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.helper = FormHelper()
+		self.helper.form_method = 'post'
+		self.helper.add_input(Submit('submit', 'submit'))
+
+
+
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Viviana/Desktop/664/HW3/heritagesites/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
